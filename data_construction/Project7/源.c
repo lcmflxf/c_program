fda55e028235cd2291357ad951f3c2fb8c241306
LiXiaaofeng <1413348475@qq.com>
2019-04-15 18:13:50 +0800

    output_linkedlist

--- data_construction/Project7/源.c
+++ data_construction/Project7/源.c
@@ -10,13 +10,13 @@ struct Student {                                    //建立Student结构体
 	float score;
 	struct Student*next;                            //指向下一个结构体
 };
+int n;                                              //n即为结点个数
 
-int n;
-
+//生成一个动态链表，并由键入赋值，最后归还首相//
 struct Student*creat(void) {                         //带回一个指针值的函数
 	struct Student*head;
 	struct Student*p1, *p2;
-	n = 0;                                           //n即为结点个数
+	n = 0;                                           
 	p1 = p2 = (struct Student *)malloc(LEN);         //开辟一个长为LEN的内存区并将其改为struct Student类型数据的指针
 	scanf_s("%ld,%f", &p1->num, &p1->score);         //导入第一个的num和score数据
 	head = NULL;
@@ -29,11 +29,25 @@ struct Student*creat(void) {                         //带回一个指针值的函数
 		scanf_s("%ld,%f", &p1->num, &p1->score);     //补充下一组数据
 	}
 	p2->next = NULL;                                 //最后，把最后一组数据对应的.next项归给NULL
-	return (head);                                   //////////////////////////
-}                                                                            //
-int main() {                                                                 //
-	struct Student*pt;                                //动态链表             //
-	pt = creat();                                                            //
-	printf("\nnum:%ld\nscore:%5.1f\n", pt->num, pt->score);//输出首项//////////
+	return (head);                                   //归还首相
+}                                   
+
+//定义输出链表//
+void output(struct Student*head) {
+	struct Student*p;
+	printf("\nNow, These %d records are :\n", n);
+	p = head;
+	if (head!=NULL)
+		do {
+			printf("%ld %5.1f\n", p->num, p->score);
+			p = p->next;
+		} while (p != NULL);
+}
+
+int main() {                                                                 
+	struct Student*head;                                //动态链表             
+	head = creat();                                                            
+	printf("\nnum:%ld\nscore:%5.1f\n", head->num, head->score);
+	output(head);
 	return 0;
 };
